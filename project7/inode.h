e6506f0f59c525235647562f17298dcc8d6e0ca9
Baleria Reyes <breyes815@gmail.com>
2024-05-18 22:16:57 -0700

    headers for new project7 functions

--- project7/inode.h
+++ project7/inode.h
@@ -1,7 +1,24 @@
 #ifndef INODE_H
 #define INODE_H
+#define INODE_PTR_COUNT 16
+#define MAX_SYS_OPEN_FILES 64
 
 int ialloc(void);
 int alloc(void);
+struct inode *incore_find_free(void);
+struct inode *incore_find(unsigned int inode_num);
+void incore_free_all(void);
+
+struct inode {
+    unsigned int size;
+    unsigned short owner_id;
+    unsigned char permissions;
+    unsigned char flags;
+    unsigned char link_count;
+    unsigned short block_ptr[INODE_PTR_COUNT];
+
+    unsigned int ref_count;  // in-core only
+    unsigned int inode_num;  // in-core only
+};
 
 #endif
