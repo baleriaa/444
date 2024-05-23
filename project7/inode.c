5c58d03ca1e230644b1cbfa9c29ecaea9eee0ea4
Baleria Reyes <breyes815@gmail.com>
2024-05-22 23:10:39 -0700

    read and write inode, iput & iget functions

--- project7/inode.c
+++ project7/inode.c
@@ -2,6 +2,7 @@
 #include "block.h"
 #include "free.h"
 #include <stdio.h>
+#include "pack.h"
 
 static struct inode incore[MAX_SYS_OPEN_FILES] = {0};
 
@@ -44,5 +45,72 @@ struct inode *incore_find(unsigned int inode_num) {
 void incore_free_all(void) {
   for (int i = 0; i < MAX_SYS_OPEN_FILES; i++) {
     incore[i].ref_count = 0;
+  }
+}
+
+void read_inode(struct inode *in, int inode_num) {
+  unsigned char block[BLOCK_SIZE] = {0};
+  int block_num = inode_num / INODES_PER_BLOCK + INODE_FIRST_BLOCK;
+  int block_offset = inode_num % INODES_PER_BLOCK;
+  int block_offset_bytes = block_offset * INODE_SIZE;
+  
+  bread(block_num, block);
+
+  in->size = read_u32(block +block_offset_bytes);
+  in->owner_id = read_u16(block + block_offset_bytes + 4);
+  in->permissions = read_u8(block + block_offset_bytes + 6);
+  in->flags = read_u8(block + block_offset_bytes + 7);
+  in->link_count = read_u8(block + block_offset_bytes + 8);
+
+  for (int i = 0; i < INODE_PTR_COUNT; i++) {
+    in->block_ptr[i] = read_u16(block + block_offset_bytes + 9 + i * 2);
+  }
+}
+
+void write_inode(struct inode *in) {
+  unsigned char block[BLOCK_SIZE] = {0};
+  int block_num = in->inode_num / INODES_PER_BLOCK + INODE_FIRST_BLOCK;
+  int block_offset = in->inode_num % INODES_PER_BLOCK;
+  int block_offset_bytes = block_offset * INODE_SIZE;
+
+  bread(block_num, block);
+
+  write_u32(block + block_offset_bytes, in->size);
+  write_u16(block + block_offset_bytes + 4, in->owner_id);
+  write_u8(block + block_offset_bytes + 6, in->permissions);
+  write_u8(block + block_offset_bytes + 7, in->flags);
+  write_u8(block + block_offset_bytes + 8, in->link_count);
+
+  for (int i = 0; i < INODE_PTR_COUNT; i++) {
+    write_u16(block + block_offset_bytes + 9 + i * 2, in->block_ptr[i]);
+  }
+  bwrite(block_num, block);
+}
+
+struct inode *iget(int inode_num) {
+  struct inode *in = incore_find(inode_num);
+  
+  if (in != NULL) {
+    in->ref_count++;
+    return in;
+  } else {
+    in = incore_find_free();
+    if (in == NULL) {
+      return NULL;
+    }
+    in->ref_count = 1;
+    in->inode_num = inode_num;
+    read_inode(in, inode_num);
+    return in;
+  }
+}
+
+void iput(struct inode *in) {
+  if(in->ref_count == 0){
+    return;
+  } 
+  in->ref_count--;
+  if (in->ref_count == 0) {
+    write_inode(in);
   }
 }
