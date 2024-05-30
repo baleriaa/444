e1516641c70e3563ce02ef4699e3442cc9c24346
Baleria Reyes <breyes815@gmail.com>
2024-05-29 23:24:34 -0700

    move structs

--- project8/dir.h
+++ project8/dir.h
@@ -1,10 +1,6 @@
 #ifndef DIR_H
 #define DIR_H
 
-struct directory *directory_open(int inode_num);
-int directory_get(struct directory *dir, struct directory_entry *ent);
-void directory_close(struct directory *d);
-
 struct directory {
   struct inode *inode;
   unsigned int offset;
@@ -14,5 +10,9 @@ struct directory_entry {
   unsigned int inode_num;
   char name[16];
 };
+
+struct directory *directory_open(int inode_num);
+int directory_get(struct directory *dir, struct directory_entry *ent);
+void directory_close(struct directory *d);
 
 #endif
